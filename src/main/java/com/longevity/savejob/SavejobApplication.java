0672881873b301a04a5b43dcecc522008de22692
최수진 <tnwlssl0729@naver.com>
2022-08-05 11:13:50 +0900

    롬복 전환

---
+++ src/main/java/com/longevity/savejob/SavejobApplication.java
@@ -0,0 +1,12 @@
+package com.longevity.savejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavejobApplication {
+    public static void main(String[] args){
+
+        SpringApplication.run(SavejobApplication.class, args);
+    }
+}
