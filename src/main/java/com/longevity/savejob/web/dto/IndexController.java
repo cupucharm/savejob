46e83d2cd64c60697dda0602554314fc99e2fe4e
최수진 <tnwlssl0729@naver.com>
2022-08-07 14:28:51 +0900

    게시글 등록

--- src/main/java/com/longevity/savejob/web/dto/IndexController.java
+++ src/main/java/com/longevity/savejob/web/dto/IndexController.java
@@ -1,8 +1,10 @@
 package com.longevity.savejob.web.dto;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
@@ -10,5 +12,10 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
